c88ef259230a606596bbf8cafdba5ae3bc9c7225
Ruaraidh <ruaraidh.leary@clyde.space>
2020-01-09 13:03:33 +0000

    gol board render done

--- Python/game_of_life/gol.py
+++ Python/game_of_life/gol.py
@@ -12,8 +12,8 @@ import random
 DEAD = 0
 LIVE = 1
 
-width = 5
-height = 5
+width = 10
+height = 8
 
 # returns a board of width x height, all dead cells
 def dead_state(width, height):
@@ -45,10 +45,38 @@ def random_state(width, height):
 #print(dead_state(width, height))
 #print(random_state(width, height))
 
+# get board witdh and height
+def get_board_width(board):
+    return len(board)
+
+def get_board_height(board):
+    return len(board[0])
+
 # render the gol board as a 2d grid, rather than printing 
 # [[0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1... # etc
 def render(board):
-    pass
+    # dict for characters to display
+    cell_display = {
+        DEAD: ' ',
+        # unicode for a filled in square.
+        LIVE: u"\u2588"
+    }
+    # creates lines[], the board to print
+    lines = list()
+    for i in range(get_board_height(board)):
+        line = str()
+        line += '| ' # vertical outline
+        for j in range(get_board_width(board)):
+            line += cell_display[board[j][i]] * 2 # without doubling cell_display it look squished
+        line += ' |' # vertical outline
+        lines.append(line)
 
-random_board = random_state(width, height)
-render(random_board)
+    horizontal_outline = str()
+    for i in range(get_board_width(board) + 2):
+        horizontal_outline += '--'
+
+    print(horizontal_outline)
+    print("\n".join(lines))
+    print(horizontal_outline)
+
+render(random_state(width, height))
