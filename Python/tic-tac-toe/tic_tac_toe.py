f6b2554d45fc69d84e47ea773ffcc0f1816947b9
Ruaraidh <ruaraidh.leary@clyde.space>
2020-01-22 13:10:44 +0000

    ttt - get_move() re implemented to work with new render()

--- Python/tic-tac-toe/tic_tac_toe.py
+++ Python/tic-tac-toe/tic_tac_toe.py
@@ -43,21 +43,43 @@ def render(board):
     print(horizontal_line)
 
 # returns the coordinates of a player's chosen move
-def get_move():
-    x = None
-    y = None
-    valid_coords = [0, 1, 2]
+def get_move(board):
+    move_int = None
+    valid_moves = get_valid_moves(board)
+    valid_moves_ints = list()
 
-    while x not in valid_coords:
-        x = int(input("Enter your move's X co-ordinate(0, 1, or 2): "))
-    while y not in valid_coords:
-        y = int(input("Enter your move's Y co-ordinate(0, 1, or 2): "))
-    
-    return (x, y)
+    for i in range(len(valid_moves)):
+        valid_moves_ints.append(CELL_DISPLAY[valid_moves[i]])
 
-#TODO is a move valid on a given board? coordinates will be valid(between
-#  0 and 2) but will the cell be empty? Can't place 'O' over 'X'
+    print("Available moves: ",', '.join(str(x) for x in valid_moves_ints))
+
+    while move_int not in valid_moves_ints:
+        move_int = input("Enter your selection: ")
+
+    # reverse CELL_DISPLAY dict to get the key from the value - this works as all kv pairs are unique
+    coords = dict(zip(CELL_DISPLAY.values(), CELL_DISPLAY.keys()))
+
+    return coords[move_int]
+
+def get_valid_moves(board):
+    valid_moves = list()
+    for x in range(BOARD_WIDTH):
+        for y in range(BOARD_HEIGHT):
+            if board[x][y] is None:
+                valid_moves.append((x, y))
+
+    return valid_moves
+
+#b1 = new_board()
+#b1[1][2] = 'X'
+#b1[0][0] = 'O'
+#b1[2][1] = 'X'
+#print(get_move(b1))
+
+
+#TODO this is not needed anymore, get_move only returns valid moves
 def is_move_valid(board, coordinates):
+    pass
 
 # make a single move, and return the updated board state
 # assume coords are correct, validation done elsewhere
@@ -66,6 +88,9 @@ def make_move(board, coord, player):
     for i in range(BOARD_HEIGHT):
         for j in range(BOARD_WIDTH):
             new_board_state[i][j] = board[i][j]
+    
+    # TODO implement new get_move()
+    valid_moves = get_valid_moves(new_board_state)
 
     x = coord[0]
     y = coord[1]
@@ -89,6 +114,7 @@ render(b1)
 
 b1 =  make_move(b1, move_coord_2, 'O')
 render(b1)
+
 
 """
 move_coords = None
