fbb1cfe1a41064801848b0f2677ffc7af5555908
Ruaraidh <ruaraidh.leary@clyde.space>
2020-02-12 15:36:07 +0000

    100 - 1, 2, 3 done

---
+++ Python/100 challenges/day 1 - 1-3/c1.py
@@ -0,0 +1,22 @@
+# Write a program which will find all such numbers which are divisible by 7 
+# but are not a multiple of 5, between 2000 and 3200 (both included).The 
+# numbers obtained should be printed in a comma-separated sequence on a 
+# single line.
+
+LOWER_RANGE_LIMIT = 2000
+UPPER_RANGE_LIMIT = 3200
+numbers_list = list()
+
+for i in range(LOWER_RANGE_LIMIT, UPPER_RANGE_LIMIT):
+    if (i % 7 == 0) and not (i % 5 ==0):
+        numbers_list.append(i)
+
+print(', '.join(str(x) for x in numbers_list))
+
+# given solution:
+"""
+for i in range(2000,3201):
+    if i%7 == 0 and i%5!=0:
+        print(i,end=',')
+print("\b")
+"""
---
+++ Python/100 challenges/day 1 - 1-3/c2.py
@@ -0,0 +1,33 @@
+# Write a program which can compute the factorial of a given numbers.The 
+# results should be printed in a comma-separated sequence on a single line
+# .Suppose the following input is supplied to the program: 8 Then, the output
+#  should be:40320
+
+import time
+
+input_number = int(input("Enter an integer: "))
+
+# recursive
+def factorial_recursive(number):
+    if number == 1:
+        return 1
+    else:
+        return number * factorial_recursive(number - 1)
+
+# iterative
+def factorial_iterative(number):
+    result = 1
+    for i in range(1, number+1):
+        result = result * i
+    return result
+
+
+recursive_start = time.time()
+print(factorial_recursive(input_number))
+recursive_end = time.time()
+print("Time for recurisve implementation: ", recursive_end - recursive_start)
+
+iterative_start = time.time()
+print(factorial_iterative(input_number))
+iterative_end = time.time()
+print("Time for iterative implementation: ", iterative_end - iterative_start)
