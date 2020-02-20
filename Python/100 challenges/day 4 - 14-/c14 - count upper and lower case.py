325b22386176030eb3a1ffc2bdba73b69bb6abed
Ruaraidh <ruaraidh.leary@clyde.space>
2020-02-20 16:53:45 +0000

    10 - c14, 15 done, day 3 folder rename

---
+++ Python/100 challenges/day 4 - 14-/c14 - count upper and lower case.py
@@ -0,0 +1,70 @@
+# Write a program that accepts a sentence and calculate the number of upper 
+# case letters and lower case letters.
+# 
+# Suppose the following input is supplied to the program:
+# 
+# Hello world!
+# Then, the output should be:
+# 
+# UPPER CASE 1
+# LOWER CASE 9
+
+upper_count = 0
+lower_count = 0
+
+input_string = input("Enter a string to count chars and digits: ")
+
+for i in input_string:
+    if i.isupper():
+        upper_count += 1
+    elif i.islower:
+        lower_count += 1
+
+print("Uppercase count: ", upper_count)
+print("Lowercase count: ", lower_count)
+
+# Given solutions
+"""
+word = input()
+upper,lower = 0,0
+
+for i in word:
+    if 'a'<=i and i<='z' :
+        lower+=1
+    if 'A'<=i and i<='Z':
+        upper+=1
+
+print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
+# OR
+
+word = input()
+upper,lower = 0,0
+
+for i in word:
+        lower+=i.islower()
+        upper+=i.isupper()
+
+print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
+# OR
+
+word = input()
+upper = sum(1 for i in word if i.isupper())           # sum function cumulatively sum up 1's if the condition is True
+lower = sum(1 for i in word if i.islower())
+
+print("UPPER CASE {0}\nLOWER CASE {1}".format(upper,lower))
+# OR
+
+# solution by Amitewu
+
+string = input("Enter the sentense")
+upper = 0
+lower = 0
+for x in string:
+    if x.isupper() == True:
+        upper += 1
+    if x.islower() == True:
+        lower += 1
+
+print("UPPER CASE: ", upper)
+print("LOWER CASE: ", lower)
+"""
---
+++ Python/100 challenges/day 4 - 14-/c15 - x+xx+xxx.py
@@ -0,0 +1,35 @@
+# Write a program that computes the value of a+aa+aaa+aaaa with a given digit 
+# as the value of a.
+# 
+# Suppose the following input is supplied to the program:
+# 
+# 9
+# Then, the output should be:
+# 
+# 11106
+
+input_integer = input("Enter an integer: ")
+
+result = int(input_integer) + int(2*input_integer) + int(3*input_integer) + int(4*input_integer)
+
+print(result)
+
+# took input as int(input()) - causing 2*input to by 18, not 99
+
+# given solutions
+"""
+a = input()
+total,tmp = 0,str()        # initialing an integer and empty string
+
+for i in range(4):
+    tmp+=a               # concatenating 'a' to 'tmp'
+    total+=int(tmp)      # converting string type to integer type
+
+print(total)
+
+# OR
+
+a = input()
+total = int(a) + int(2*a) + int(3*a) + int(4*a)  # N*a=Na, for example  a="23", 2*a="2323",3*a="232323"
+print(total)
+"""
