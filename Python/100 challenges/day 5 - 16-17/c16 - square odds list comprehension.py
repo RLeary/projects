97b564b797b19b7d4972008f060fbfeba6dd1517
Ruaraidh <ruaraidh.leary@clyde.space>
2020-02-21 12:51:32 +0000

    100 - c16, 17 done, day 4 folder rename

---
+++ Python/100 challenges/day 5 - 16-17/c16 - square odds list comprehension.py
@@ -0,0 +1,25 @@
+# Use a list comprehension to square each odd number in a list. The list is 
+# input by a sequence of comma-separated numbers. Suppose the following input 
+# is supplied to the program:
+# 
+# 1,2,3,4,5,6,7,8,9
+# Then, the output should be:
+# 
+# 1,9,25,49,81
+
+input_list = input("Enter a comma separated list of integers: ").split(',')
+
+input_list_ints = [int(i) for i in input_list]
+
+squared_odds = [i*i for i in input_list_ints if i % 2 == 1]
+
+print(squared_odds)
+
+output_list = [str(i) for i in squared_odds]
+print(','.join(output_list))
+
+# given solution
+"""
+lst = [str(int(i)**2) for i in input().split(',') if int(i) % 2]
+print(",".join(lst))
+"""
---
+++ Python/100 challenges/day 5 - 16-17/c17 - deposit-withdraw.py
@@ -0,0 +1,82 @@
+# Write a program that computes the net amount of a bank account based a
+# transaction log from console input. The transaction log format is shown as 
+# following:
+# 
+# D 100
+# W 200
+# D means deposit while W means withdrawal.
+# Suppose the following input is supplied to the program:
+# 
+# D 300
+# D 300
+# W 200
+# D 100
+# Then, the output should be:
+# 
+# 500
+balance = 0
+
+while True:
+    input_command = input(
+        "Enter D or W followed by space, then the value: ").split(' ')
+    if len(input_command) > 2:
+        print("Invalid command, exiting")
+        break
+    if input_command[0] == 'D' or 'd':
+        balance += int(input_command[1])
+    elif input_command[0] == 'W' or 'w':
+        balance -= int(input_command[1])
+    else:
+        print('invalid input, exiting')
+        break
+print(balance)
+
+# given solutions
+"""
+total = 0
+while True:
+    s = input().split()
+    if not s:            # break if the string is empty
+        break
+    cm,num = map(str,s)    # two inputs are distributed in cm and num in string data type
+
+    if cm=='D':
+        total+=int(num)
+    if cm=='W':
+        total-=int(num)
+
+print(total)
+
+'''Solution by: leonedott'''
+
+lst = []
+while True:
+  x = input()
+  if len(x)==0:
+    break
+  lst.append(x)
+
+balance = 0
+for item in lst:
+  if 'D' in item:
+    balance += int(item.strip('D '))
+  if 'W' in item:
+    balance -= int(item.strip('W '))
+print(balance)
+
+'''Solution by: AlexanderSro'''
+
+account = 0
+while True:
+    action = input("Deposit/Whitdrow/Balance/Quit? D/W/B/Q: ").lower()
+    if action == "d":
+        deposit = input("How much would you like to deposit? ")
+        account = account + int(deposit)
+    elif action == "w":
+        withdrow = input("How much would you like to withdrow? ")
+        account = account - int(withdrow)
+    elif action == "b":
+        print(account)
+    else:
+        quit()
+"""
